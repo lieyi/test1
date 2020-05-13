883c4e53e6ba993965e79bea7b2ee8df3dd4bb02
lieyi <893001864@qq.com>
2020-05-13 21:30:30 +0800

    xxx

---
+++ 3.py
@@ -0,0 +1,18 @@
+import os
+import sys
+from PIL import ImageGrab
+import time
+
+save_path = "D:\\1\\test1\\1.jpg";
+cmd1 = "git add " + save_path
+cmd2 = "git commit -m 'test'"
+cmd3 = "git push origin master"
+
+
+while(True):
+    image = ImageGrab.grab()
+    image.save(save_path)
+    os.system(cmd1)
+    os.system(cmd2)
+    os.system(cmd3)
+    time.sleep(300)
